52334ef732d7aac8f1ce36f6fe63e925babcf5ae
VISHNU23CSB61 <153609450+VISHNU23CSB61@users.noreply.github.com>
2026-06-22 18:31:02 +0530

    Initialized LostLines React project

---
+++ lostlines-react/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>LostLines React</h1>
+        <p>Campus Lost & Found Dashboard</p>
+      </header>
+
+      <main>
+        <section>
+          <h2>React Version Started</h2>
+          <p>This is the first React step of LostLines.</p>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
