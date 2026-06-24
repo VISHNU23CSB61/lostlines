b2765bff30a5c7776fa4da6ca4519ec31bf48c28
VISHNU23CSB61 <153609450+VISHNU23CSB61@users.noreply.github.com>
2026-06-24 11:11:37 +0530

    added itemlist.jsx component

---
+++ lostlines-react/src/components/ItemList.jsx
@@ -0,0 +1,17 @@
+function ItemList({ items }) {
+    if(items.length===0){
+        return <p> No items reported</p>
+    }
+    return(
+        <ul>
+            {items.map((item) => (
+                <li key={item.id}>
+                    <strong>{item.name}</strong>
+                    <p>Location: {item.location}</p>
+                    <p>Status: {item.status}</p>
+                </li>
+            ))}
+        </ul>
+    );
+}
+export default ItemList;
