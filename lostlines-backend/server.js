0cb3e29e3cfc7ce5fcac33112a70ccceaf044522
VISHNU23CSB61 <153609450+VISHNU23CSB61@users.noreply.github.com>
2026-07-02 16:22:14 +0530

    connected MongoDb

--- lostlines-backend/server.js
+++ lostlines-backend/server.js
@@ -8,7 +8,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
